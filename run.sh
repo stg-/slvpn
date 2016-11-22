ff2ee482e239a9081c92b09ee8ff786c2b26eb46
Santiago Sanchez Paz <santiago.sanchezpaz@es.ibm.com>
2016-11-22 12:41:38 +0100

    keep alive the vpn

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/usr/local/array_vpn/array_vpnc64 -host ${VPN_HOST} -user ${VPN_USER} -passwd ${VPN_PASS} -port ${VPN_PORT}
+while true; do
+	ps aux | grep -v grep | grep array_vpnc64 >/dev/null
+	if [ $? -ne 0 ]; then 
+		nohup /usr/local/array_vpn/array_vpnc64 -host ${VPN_HOST} -user ${VPN_USER} -passwd ${VPN_PASS} -port ${VPN_PORT} & 
+	else 
+		sleep 10; 
+	fi; 
+done
 
